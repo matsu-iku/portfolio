a431ead868e16847b59ae4c86b6ec1cbf4f65f12
matsu-iku <80937961+matsu-iku@users.noreply.github.com>
2021-04-04 17:19:45 +0900

    php変更

--- test.php
+++ test.php
@@ -1,3 +1,8 @@
 <?php
-echo __FILE__;
+echo $_SERVER['REQUEST_URI'];
+echo "<br>";
+echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+echo "<br>";
+echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") .
+$_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 ?>
