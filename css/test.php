5df81a78bf7606be1c216dd856a223636b82a23b
matsu-iku <80937961+matsu-iku@users.noreply.github.com>
2021-04-22 10:39:39 +0900

    test.php削除

--- css/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo $_SERVER['REQUEST_URI'];
-echo "<br>";
-echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-echo "<br>";
-echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") .
-$_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-?>
